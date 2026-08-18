2f1f3cafdee530908aa1898a835f3c8e3a2ac1b3
_AcegnoK_ <mrkkomaroffoleg8554@gmail.com>
2026-08-18 23:12:07 +0300

    Add Upload-Post integration as unified posting platform

--- config.py
+++ config.py
@@ -18,4 +18,8 @@ IG_ACCESS_TOKEN = os.getenv("IG_ACCESS_TOKEN")
 IG_USER_ID = os.getenv("IG_USER_ID")
 IG_PUBLIC_URL_BASE = os.getenv("IG_PUBLIC_URL_BASE")
 
+UPLOAD_POST_API_KEY = os.getenv("UPLOAD_POST_API_KEY")
+UPLOAD_POST_USER = os.getenv("UPLOAD_POST_USER")
+UPLOAD_POST_PLATFORMS = os.getenv("UPLOAD_POST_PLATFORMS", "youtube,tiktok,instagram")
+
 os.makedirs(UPLOADS_DIR, exist_ok=True)
